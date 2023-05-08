8a8426020470d8e5d863c1ce27bf6e6d65bf50a3
DylanWGordon <dw.Gordon@gmail.com>
2023-05-08 15:43:09 -0700

    started gordy/project/firstGame

---
+++ projects/gordon/firstGame/firstGame.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas');
+const pA = canvas.getContext('2d');
+
+let locX = 250;
+let locY = 250;
+pA.fillStyle = 'blue';
+pA.fillRect(locX, locY, 20, 20)
+pa
