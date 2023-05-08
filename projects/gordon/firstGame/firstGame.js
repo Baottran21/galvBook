d194d72fe04fba3a6cbfa3f7bc7d5ab81ac001a6
DylanWGordon <dw.Gordon@gmail.com>
2023-05-08 16:00:31 -0700

    removed butts.

--- projects/gordon/firstGame/firstGame.js
+++ projects/gordon/firstGame/firstGame.js
@@ -4,5 +4,6 @@ const pA = canvas.getContext('2d');
 let locX = 250;
 let locY = 250;
 pA.fillStyle = 'blue';
-pA.fillRect(locX, locY, 20, 20)
-pa
+pA.fillRect(locX, locY, 20, 20);
+pA.strokeRect(50, 50, 50, 50)
+
