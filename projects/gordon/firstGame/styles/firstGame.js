dacdd0592ae1cb685f803658b8dfccfd29ccb3da
DylanWGordon <dw.Gordon@gmail.com>
2023-05-08 16:31:21 -0700

    bit of a base framework for gordy/firstGame. plenty of bugs left to squash

--- projects/gordon/firstGame/styles/firstGame.js
+++ projects/gordon/firstGame/styles/firstGame.js
@@ -3,7 +3,9 @@ const pA = canvas.getContext('2d');
 
 let locX = 250;
 let locY = 250;
+let baseX = Math.floor(Math.random()*475);
+let baseY = Math.floor(Math.random()*475);
 pA.fillStyle = 'blue';
 pA.fillRect(locX, locY, 20, 20);
-pA.strokeRect(50, 50, 50, 50)
+pA.strokeRect(baseX, baseY, 50, 50)
 
